f5b1f199a5e7b89597f79a420267d11e47fbb0aa
Beverly Abadines <BeverlyAb@users.noreply.github.com>
2016-09-18 22:43:42 -0700

    Update Ave_Var.cpp

--- Ave_Var.cpp
+++ Ave_Var.cpp
@@ -10,47 +10,47 @@
 
 using namespace std;
 
-	float average(float container[]) { // Calculates and returns the average of numbers in an array 		
-		float average_value = 0;
+float average(float container[]) { // Calculates and returns the average of numbers in an array 		
+	float average_value = 0;
 
-		for(int i = 0; i < ARRAY_SIZE; i++) {
-			average_value += container[i];
-		}
-		average_value /= ARRAY_SIZE;
-		return average_value;
-	} //average
+	for(int i = 0; i < ARRAY_SIZE; i++) {
+		average_value += container[i];
+	}
+	average_value /= ARRAY_SIZE;
+	return average_value;
+} //average
 		
-	float variance(float container[]) { // Calculates and returns the variance of numbers in an array
-		float variance_value = 0;
-		float average_value = average(container);
+float variance(float container[]) { // Calculates and returns the variance of numbers in an array
+	float variance_value = 0;
+	float average_value = average(container);
 
-		for(int i = 0; i < ARRAY_SIZE; i++) {
-			variance_value += (pow(container[i] - average_value, 2));
-		} 
-		variance_value /= (ARRAY_SIZE - 1);
-		return variance_value;
-	} // variance
+	for(int i = 0; i < ARRAY_SIZE; i++) {
+		variance_value += (pow(container[i] - average_value, 2));
+	} 
+	variance_value /= (ARRAY_SIZE - 1);
+	return variance_value;
+} // variance
 
-	void populateArray(float container[]) { // Populates array with user inputted floats; +10 is buffer
-		float input = 0;		
-		int counter = 0;
-		cout << "Enter your values one by one.\n";
+void populateArray(float container[]) { // Populates array with user inputted floats; +10 is buffer
+	float input = 0;		
+	int counter = 0;
+	cout << "Enter your values one by one.\n";
+	cin >> input;
+		
+	while(counter < (ARRAY_SIZE - 1)) {
+		container[counter++] = input; 
 		cin >> input;
-		
-		while(counter < (ARRAY_SIZE - 1)) {
-			container[counter++] = input; 
-			cin >> input;
-		}
-		container[counter] = input; 
-	} // populateArray
+	}
+	container[counter] = input; 
+} // populateArray
 
-	void display(float container[]) { // Displays all the floats in array
-		cout << "Your values are:\n";
+void display(float container[]) { // Displays all the floats in array
+	cout << "Your values are:\n";
 
-		for(int i = 0; i < ARRAY_SIZE; i++) {
-			cout << container[i] << endl;
-		}
-	} // display
+	for(int i = 0; i < ARRAY_SIZE; i++) {
+		cout << container[i] << endl;
+	}
+} // display
 		
 int main()
 { 

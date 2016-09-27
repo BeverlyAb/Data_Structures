aa555414214f008cb06710a7f84e108d917cea60
Beverly Abadines <BeverlyAb@users.noreply.github.com>
2016-09-27 11:58:47 -0700

    Update stest.cpp

--- stest.cpp
+++ stest.cpp
@@ -9,13 +9,13 @@ using namespace std;
 main()
 {
 	Stack<int> s;      		// uses List as the default container
-	s.push(5);				// front/back [5]
-	s.push(6);				// front/back [6, 5]
-	cout << s.top() << endl;// should return 6
+	s.push(5);			// front/back [5]
+	s.push(6);			// front/back [6, 5]
+	cout << s.top() << endl;	// should return 6
 
-	Stack<double, vector> v;// uses Vector as the container
+	Stack<double, vector> v;	// uses Vector as the container
 	v.push(1.5);			// front/back [1.5]
 	v.push(2.3);			// front/back [2.3, 1.5]
-	v.pop();				// front/back [1.5]
-	cout << v.top() << endl;// should return 1.5
+	v.pop();			// front/back [1.5]
+	cout << v.top() << endl;	// should return 1.5
 }

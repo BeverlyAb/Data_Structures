5bd04b96c6ae5bac5b8d99474d82b43bae3ed8fd
babadine <bev.abadines@gmail.com>
2020-10-17 22:35:39 -0700

    Search
    go needs its own folder. needed to create copy and add to dir

---
+++ Trie_DS.go
@@ -0,0 +1,25 @@
+package Trie_DS
+
+import (
+	"fmt"
+	"TrieNode"
+)
+
+type Trie{
+	children = map[byte]TrieNode
+	root TrieNode
+}
+
+func (t * TrieNode)TrieNode(){
+	children := make(map[byte]TrieNode)
+	endOfWord := false
+}
+
+func (t * TrieNode)find() bool{
+
+	return false;
+}
+
+func (t * TrieNode) insert(){
+
+}

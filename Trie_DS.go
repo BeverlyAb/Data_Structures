2aee9fbc63153a6c5a8bc4f1de0cfdc30d4e390e
babadine <bev.abadines@gmail.com>
2020-10-17 23:35:49 -0700

    Insert works

--- Trie_DS.go
+++ Trie_DS.go
@@ -23,32 +23,47 @@ func (t * Trie)Trie(){
 }
 
 func (t * Trie)search(current TrieNode, word string, index int) bool{
-	if index == len(word) {
+	if index == len(word){
 		return current.endOfWord
 	}
 
-	var ch byte = word[index]
+	var ch byte= word[index]
 	node, found := current.children[ch]
-	if found {
-		return t.search(node, word, index + 1)
-	} else {
+	if !found {
 		return false
-	}
+	} 
+	return t.search(node, word, index + 1)
 }
 
 func (t * Trie) Search(word string)bool{
 	return t.search(t.root, word, 0)
 }
 
+func (t * Trie)insert(current TrieNode, word string, index int){
+	if index == len(word){
+		current.endOfWord = true
+		return
+	}	
+	var ch byte = word[index]
+	node, found := current.children[ch]
 
+	if !found {
+		node.TrieNode()
+		if index == len(word)-1{
+			node.endOfWord = true
+		}
+		current.children[ch] = node
+	}
+	t.insert(node, word, index+1)
+}
 
-// func (t * Trie) insert(){
-
-// }
+func (t * Trie)Insert(word string){
+	t.insert(t.root, word, 0)	
+}
 
 func main() {
 	var test Trie
 	test.Trie()
-	fmt.Println(test.Search("root"))
-	
+	test.Insert("abcd")
+	fmt.Println(test.Search("abcd"))
 }
